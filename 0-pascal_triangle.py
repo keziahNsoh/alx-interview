5b78d77bcb37e8ed8422c4385ac1466c8b1ed29b
keziahNsoh <porepeya@gmail.com>
2024-10-03 19:23:23 -0700

    third trial

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,17 +3,17 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []  # Initialize the triangle as an empty list
+    triangle = []  # Initialization of the triangle as an empty list
 
     for i in range(n):
-        # Create a new row initialized with 1s
+        # This creates a new row initialized with 1s
         row = [1] * (i + 1)
 
-        # Calculate the values for the inner elements of the row
+        # Calculations of the values for the inner elements of the row
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
-        triangle.append(row)  # Append the row to the triangle
+        triangle.append(row)  # This append the row to the triangle
 
     return triangle
 
