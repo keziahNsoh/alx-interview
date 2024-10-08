4cec92892f8bd8c70a97298e09553aac09d11588
keziahNsoh <porepeya@gmail.com>
2024-10-08 14:53:53 -0700

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,25 @@
+#!/usr/bin/python3
+"""
+pascal's triangle
+"""
+
+
 def pascal_triangle(n):
+    """This is a function for making pascal's triangle in a form of lists
+    Args:
+        - n: the number of rows in the triangle
+    Returns:
+        - list of lists in case of n > 0
+        - empty list in case of n <= 0
+    """
+    pascal = []
     if n <= 0:
-        return []
-
-    triangle = []
+        return pascal
     for i in range(n):
-        row = [1] * (i + 1)
-        
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
-        triangle.append(row)
+        pre = [(1 if j == 0 or j == i else
+                pascal[i - 1][j] + pascal[i - 1][j - 1])
+               for j in range(i + 1)]
+        pascal.append(pre)
 
-    return triangle
+    return pascal
 
