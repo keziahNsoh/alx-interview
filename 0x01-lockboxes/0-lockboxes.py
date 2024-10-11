e231f5a31b62f13306ee55a85ed60c3ff4caa47b
keziahNsoh <porepeya@gmail.com>
2024-10-10 17:41:12 -0700

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
+"""
+Module for determining if all boxes can be opened.
+
+The function canUnlockAll takes a list of boxes, where each box contains
+keys to other boxes. The first box is initially unlocked.
+
+Returns True if all boxes can be opened, otherwise returns False.
+"""
+
 
 def canUnlockAll(boxes):
-    visited = set()
-    stack = [0]
-    
-    while stack:
-        box = stack.pop()
-        
-        if box not in visited:
-            visited.add(box)
-            
-            for key in boxes[box]:
-                if key not in visited and key < len(boxes):
-                    stack.append(key)
-    
-    return len(visited) == len(boxes)
+    """
+    Determine if all boxes can be opened.
 
+    Args:
+        boxes (list of list of int): The list of boxes with their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # The first box is unlocked
+    stack = [0]  # Start with the first box
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < n and not visited[key]:  # Check if the key is valid
+                visited[key] = True  # Mark the box as visited
+                stack.append(key)  # Add it to the stack to explore later
+
+    return all(visited)
--- 0x01-lockboxes/README.md
+++ 0x01-lockboxes/README.md
@@ -1,7 +1,5 @@
 # Lockboxes
 
-This project implements a function to determine if all locked boxes can be opened given a specific set of keys contained within each box.
-
 ## Description
 
-I have `n` locked boxes, each containing keys to other boxes. The goal is to check if all boxes can be opened, starting from the first box, which is initially unlocked.
+This project includes a Python function to determine if all locked boxes can be opened given a list of boxes containing keys. The first box is initially unlocked.
