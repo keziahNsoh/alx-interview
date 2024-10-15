4c42d8f6de16ef3736039ac0244b0a811f392bb9
keziahNsoh <porepeya@gmail.com>
2024-10-15 14:35:06 -0700

    push

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Module to calculate minimum operations to achieve n characters."""
 
 
 def minOperations(n):
-    """Calculate the fewest number of operations
-    to reach exactly n characters."""
+    """Calculate the fewest number of operations to reach exactly n characters."""
     if n <= 1:
         return 0
 
@@ -13,7 +13,7 @@ def minOperations(n):
     # Factor n and count operations based on prime factors
     while n > 1:
         if n % factor == 0:
-            operations += factor  # We need `factor` operations (Copy + Pasts)
+            operations += factor  # We need `factor` operations
             n //= factor  # Reduce n by the factor
         else:
             factor += 1  # Move to the next possible factor
