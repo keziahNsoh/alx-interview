62385d8fca2ef1deb7b14dd015bf35c151f6cf8c
keziahNsoh <porepeya@gmail.com>
2024-10-15 13:58:58 -0700

    0x02-minimum_operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """Calculate the fewest number of operations
+    to reach exactly n characters."""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    # Factor n and count operations based on prime factors
+    while n > 1:
+        if n % factor == 0:
+            operations += factor  # We need `factor` operations
+            n //= factor  # Reduce n by the factor
+        else:
+            factor += 1  # Move to the next possible factor
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# Minimum Operations
+
+This project contains a Python solution for calculating the minimum number of operations required to achieve exactly `n` characters in a text file using only "Copy All" and "Paste" operations.
