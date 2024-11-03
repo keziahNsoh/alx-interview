edd6433310e7198f481923be958de14ad5abefb6
keziahNsoh <porepeya@gmail.com>
2024-11-03 16:52:00 +0000

    fix lint

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-
+"""
+It shows if a given data set represents a valid UTF-8 encoding
+"""
 
 def validUTF8(data):
-    # It shows if a given data set represents a valid UTF-8 encoding
+    """
+    It shows if a given data set represents a valid UTF-8 encoding
+    """
     expected_bytes = 0
 
     for number in data:
         if expected_bytes != 0:
             # We are expecting continuation bytes
-            if not (128 <= number < 192):  # 10xxxxxx
+            if not 128 <= number < 192:  # 10xxxxxx
                 return False
             expected_bytes -= 1
         else:
             if number < 128:  # 0xxxxxxx
                 continue
-            elif 192 <= number < 224:  # 110xxxxx
+            if 192 <= number < 224:  # 110xxxxx
                 expected_bytes = 1
             elif 224 <= number < 240:  # 1110xxxx
                 expected_bytes = 2
