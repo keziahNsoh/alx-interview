1d93a77741d965d0d02da2a5af13b7a2f570cbfe
keziahNsoh <porepeya@gmail.com>
2025-01-06 08:33:40 -0800

    fourth correction push

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,8 +16,8 @@ def main():
     total_size = 0
     line_count = 0
     status_codes = {
-        200: 0, 301: 0, 400: 0, 401: 0,
-        403: 0, 404: 0, 405: 0, 500: 0
+            200: 0, 301: 0, 400: 0, 401: 0,
+            403: 0, 404: 0, 405: 0, 500: 0
     }
 
     try:
@@ -27,16 +27,20 @@ def main():
             try:
                 parts = line.split()
                 # Check log format
-                if len(parts) != 9 or parts[5] != '"GET' or \
-                   parts[6] != '/projects/260' or parts[7] != 'HTTP/1.1"':
+                if (
+                    len(parts) != 9
+                    or parts[5] != '"GET'
+                    or parts[6] != "/projects/260"
+                    or parts[7] != 'HTTP/1.1"'
+                ):
                     continue
                 status_code = int(parts[-2])
                 file_size = int(parts[-1])
-                
+
                 if status_code in status_codes:
                     status_codes[status_code] += 1
                 total_size += file_size
-                
+
                 if line_count % 10 == 0:
                     print_stats(total_size, status_codes)
             except (ValueError, IndexError):
